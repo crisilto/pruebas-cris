7a41f4887acfb6b71202d23202adb4fbfec77915
LablancaDev <valenciano_david@hotmail.com>
2024-01-19 22:17:55 +0100

    Añadida interacción del usuario para saludo personalizado

--- PruebasGit/App.java
+++ PruebasGit/App.java
@@ -1,8 +1,13 @@
 package PruebasGit;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese su nombre: ");
+        String nombre = scanner.nextLine();
+        System.out.println("Hello, " + nombre + "!");
+        scanner.close();
     }
-    
 }
